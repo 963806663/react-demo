645d82eeb20a03fbf5a257ee23610ebe95a16b2a
许岩 <xuyan@lenztechretail.com>
2018-11-06 15:12:09 +0800

    inital commit

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import '../css/header.css';
+
+function SiteIndex(props) {
+    return <header>Hello, {props.name}</header>;
+}
+export default SiteIndex;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Header from './components/header.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const element = <Header name="首页" />;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)
+
 serviceWorker.unregister();
